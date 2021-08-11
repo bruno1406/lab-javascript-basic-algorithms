46d66d51bbdf1266f722dddcf03de118ef43961d
Bruno Barbagelata <bruno140685@gmail.com>
2021-08-11 12:50:51 -0400

    iteration 1, 2 and first part of iteration 3

--- js/index.js
+++ js/index.js
@@ -1,7 +1,27 @@
 // Iteration 1: Names and Input
 
+ // Iteration 1: Names and Input
+//1.1 Create a variable `hacker1` with the driver's name.
+var hacker1 = "Michael"
+//1.2 Print `"The driver's name is XXXX"`.
+console.log(`The driver's name is ${hacker1}`)
+//1.3 Create a variable `hacker2` with the navigator's name.
+var hacker2 = "Dragan and Bruno"
+//1.4 Print `"The navigator's name is YYYY"`.
+console.log(`the navigator's name is ${hacker2}`)
+
 
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
+  } else if ( hacker2.length > hacker1.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+  } else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+  }
 
 // Iteration 3: Loops
+
+console.log(`${hacker1.toUpperCase().split('').join(' ')}`)
+
